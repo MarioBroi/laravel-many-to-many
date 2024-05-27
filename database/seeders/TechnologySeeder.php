fbf91a20556924cd56731b8586fd132e4ff4bdb0
MarioBroi <inoix16@gmail.com>
2024-05-27 15:19:15 +0100

    add columns to table, add table seeder for technology. Edit database seeder with call for technology

--- database/seeders/TechnologySeeder.php
+++ database/seeders/TechnologySeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Technology;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class TechnologySeeder extends Seeder
 {
@@ -12,6 +13,13 @@ class TechnologySeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $technologies = ['html', 'css', 'php', 'vue', 'js', 'ml', 'ai', 'gpt', 'gemini'];
+
+        foreach ($technologies as $technology) {
+            $newTechnology = new Technology();
+            $newTechnology->name = $technology;
+            $newTechnology->slug = Str::slug($newTechnology->name, '-');
+            $newTechnology->save();
+        }
     }
 }
